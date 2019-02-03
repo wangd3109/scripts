84d73f914d6f978dc39b458d64d06813da710342
Duo Wang <wangd3109@gmail.com>
2019-02-03 22:55:18 +0100

    add the "if" part

--- band/eachBand.sh
+++ band/eachBand.sh
@@ -5,20 +5,43 @@ knpoint=$(awk 'NR==6 {print $2}' EIGENVAL)
 
 sed  '1,7d' EIGENVAL >tmp_eigenval
 
-for i in `seq 1 1 9`
-do
-	awk '/^    '$i'/{print $0}' tmp_eigenval > band$i
-done
 
-for j in `seq 10 1 99`
-do
-	awk '/^   '$j'/{print $0}' tmp_eigenval >band$j
-done
+if [ $nband -gt 99 ]
+then
+	for i in `seq 1 1 9`
+	do
+		awk '/^    '$i'/{print $0}' tmp_eigenval > band$i
+	done
 
-for n in `seq 100 1 $nband`
-do
-	awk '/^  '$n'/{print $0}' tmp_eigenval >band$n
-done
+	for j in `seq 10 1 99`
+	do
+		awk '/^   '$j'/{print $0}' tmp_eigenval >band$j
+	done
+
+	for n in `seq 100 1 $nband`
+	do
+		awk '/^  '$n'/{print $0}' tmp_eigenval >band$n
+	done
+elif [ $nband -lt 10 ]
+then
+	for i in `seq 1 1 $nband`
+	do
+		awk '/^    '$i'/{print $0}' tmp_eigenval > band$i
+	done
+else
+	for i in `seq 1 1 9`
+	do
+		awk '/^    '$i'/{print $0}' tmp_eigenval > band$i
+	done
+
+	for j in `seq 10 1 $nband`
+	do
+		awk '/^   '$j'/{print $0}' tmp_eigenval >band$j
+	done 
+fi
+
+
+
 
 for k in `seq 1 1 $nband` # specify the fermi-level by hand
 do
@@ -28,8 +51,8 @@ done
 
 for l in `seq 1 1 $nband` # spin polarization or not
 do
-	echo "'nband$l'" u 1:2 w l lt 8 lw 3 lc 0 t '""' ,'\' >> band.gnu
-#	echo "'nband$l'" u 1:3 w l lt 8 lw 3 lc 1 t '""' ,'\' >> band.gnu
+	echo "'nband$l'" u 1:2 w l lt 8 lw 3 lc 0 t '""' ,'\' >> plotband.gnu
+#	echo "'nband$l'" u 1:3 w l lt 8 lw 3 lc 1 t '""' ,'\' >> plotband.gnu
 done
 
 rm tmp_eigenval band*
