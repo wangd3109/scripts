8f627733684fed2d2d9d485cdd3e01dfad7e4588
Duo Wang <wangd3109@gmail.com>
2019-02-03 21:45:31 +0100

    intial script for band ploting

---
+++ band/eachBand.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+nband=$(awk 'NR==6 {print $3}' EIGENVAL)
+knpoint=$(awk 'NR==6 {print $2}' EIGENVAL)
+
+sed  '1,7d' EIGENVAL >tmp_eigenval
+
+for i in `seq 1 1 9`
+do
+	awk '/^    '$i'/{print $0}' tmp_eigenval > band$i
+done
+
+for j in `seq 10 1 99`
+do
+	awk '/^   '$j'/{print $0}' tmp_eigenval >band$j
+done
+
+for n in `seq 100 1 $nband`
+do
+	awk '/^  '$n'/{print $0}' tmp_eigenval >band$n
+done
+
+for k in `seq 1 1 $nband` # specify the fermi-level by hand
+do
+	awk '{print NR-1, $2+2.79093985,$3+2.79093985}' band$k > nband$k
+done
+
+
+for l in `seq 1 1 $nband` # spin polarization or not
+do
+	echo "'nband$l'" u 1:2 w l lt 8 lw 3 lc 0 t '""' ,'\' >> band.gnu
+#	echo "'nband$l'" u 1:3 w l lt 8 lw 3 lc 1 t '""' ,'\' >> band.gnu
+done
+
+rm tmp_eigenval band*
